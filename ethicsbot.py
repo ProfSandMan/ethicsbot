5ac3eacc5b85e1cec78e846de635bc74d3eed598
Sheboyganaire <123134257+Sheboyganaire@users.noreply.github.com>
2026-01-22 21:37:29 -0600

    error trapped, changed download to single button no popup, added some common API errors

--- ethicsbot.py
+++ ethicsbot.py
@@ -3,6 +3,7 @@ import json
 import time
 
 import pandas as pd
+import openai
 import streamlit as st
 
 import frontend.css as css
@@ -17,7 +18,7 @@ from backend.agents import (
     InjectionAttackAgent, 
     ConductorAgent)
 
-version = '1.0.4'
+version = '1.0.5'
 MODEL = 'gpt-4-mini'
 
 # ========================================================================================================================
@@ -36,28 +37,6 @@ def get_username():
         else:
             st.session_state['username'] = tempuser.lower()
             st.rerun()
-
-def callback():
-    st.session_state['downloaded'] = True
-    st.balloons()
-
-@st.dialog("Download Conversation")
-def package_and_download():
-    # Package
-    package = {
-        'username': st.session_state['username'],
-        'occupation': st.session_state['occupation'],
-        'topic': st.session_state['topic'],
-        'messages': st.session_state['messages'],
-        'start_time': st.session_state['start_time'],
-        'end_time': st.session_state['end_time'],
-    }
-    package = json.dumps(package)
-    st.download_button('Download D2L File', package, f'{st.session_state["username"].replace("@marquette.edu", "").replace(".", "-")} {datetime.now().strftime('%d/%m/%Y, %H:%M:%S')}.json',
-                        on_click=callback, type='primary')
-    # st.download_button('Download Conversation', st.session_state['messages'], 
-    #                    f'Ethics Conversation {datetime.now().strftime('%d/%m/%Y, %H:%M:%S')}.txt',
-    #                    on_click=callback)
 
 # ========================================================================================================================
 # Establish app and session_state variables
@@ -83,8 +62,6 @@ if "occupation" not in st.session_state:
     st.session_state['occupation'] = None
 if "topic" not in st.session_state:
     st.session_state['topic'] = None
-if "downloaded" not in st.session_state:
-    st.session_state['downloaded'] = False
 if "user_avatar" not in st.session_state:
     st.session_state['user_avatar'] = AVATAR
 
@@ -106,11 +83,6 @@ with st.sidebar:
 if st.session_state['username'] is None:
     get_username()
 
-if st.session_state['downloaded'] == True:
-    st.session_state.messages = []
-    st.session_state['user_launched_convo'] = False
-    st.session_state['downloaded'] = False
-
 # ========================================================================================================================
 # Build Header and inputs
 st.header("⚖️ ADV EthicsBot")
@@ -120,18 +92,25 @@ col1, col2, col3, col4 = st.columns([1,1,1,3])
 
 # ========================================================================================================================
 # Export
-if col3.button("Export Conversation", type='primary'):
-    st.session_state['end_time'] = time.time()
-    # Retain conversation in window
-    for message in st.session_state.messages:
-        if message["role"].lower().strip() != 'system':
-            if message["role"] == "user":
-                use_avatar = st.session_state['user_avatar']
-            else:
-                use_avatar = "⚖️"
-            with st.chat_message(message["role"], avatar=use_avatar):
-                st.write(message["content"])
-    package_and_download()
+st.session_state['end_time'] = time.time()
+export_package = {
+    'username': st.session_state['username'],
+    'occupation': st.session_state['occupation'],
+    'topic': st.session_state['topic'],
+    'messages': st.session_state['messages'],
+    'start_time': st.session_state['start_time'],
+    'end_time': st.session_state['end_time'],
+}
+export_json = json.dumps(export_package, indent=2)
+download_filename = f'{st.session_state["username"].replace("@marquette.edu", "").replace(".", "-")} {datetime.now().strftime("%d-%m-%Y_%H-%M-%S")}.json'
+
+col3.download_button(
+    label='Download D2L File',
+    data=export_json,
+    file_name=download_filename,
+    mime='application/json',
+    type='primary'
+)
 
 # Reset
 if col2.button("Reset Conversation", type='secondary'):
@@ -151,7 +130,7 @@ if col1.button("Begin Conversation", type='primary'):
 
 # Create llm
 if st.session_state['llm'] is None and st.session_state['api_key'] is not None:
-    st.session_state['llm'] = OpenAILLM(api_key=openaikey, model = MODEL)
+    st.session_state['llm'] = OpenAILLM(api_key=st.session_state['api_key'], model = MODEL)
 
 # Begin conversation
 if viable == True or st.session_state['user_launched_convo'] == True:
@@ -164,57 +143,101 @@ if viable == True or st.session_state['user_launched_convo'] == True:
             st.session_state['occupation'] = occupation
             st.session_state['topic'] = topic
             scenario = build_scenario_prompt(occupation = occupation, topic = topic)
-            scenario_agent = ScenarioAgent(llm = st.session_state['llm'])
-            scenario_agent.system_prompt_ += prompt_modifier(st.session_state['username'])
-            scenario = scenario_agent.respond(scenario)
+            try:
+                scenario_agent = ScenarioAgent(llm = st.session_state['llm'])
+                scenario_agent.system_prompt_ += prompt_modifier(st.session_state['username'])
+                scenario = scenario_agent.respond(scenario)
+                st.session_state.messages.append({"role": "assistant", "content": scenario})
+                st.session_state['user_launched_convo'] = True
+                st.session_state['start_time'] = time.time()
 
-            st.session_state.messages.append({"role": "assistant", "content": scenario})
-            st.session_state['user_launched_convo'] = True
-            st.session_state['start_time'] = time.time()
+                # Add to chat log
+                with st.chat_message("assistant", avatar = "⚖️"):
+                    st.write(scenario)       
 
-        # Add to chat log
-        with st.chat_message("assistant", avatar = "⚖️"):
-            st.write(scenario)        
+            except Exception as e:
+                # if e is openAI.AuthenticationError, then prompt user to enter API key
+                if isinstance(e, openai.AuthenticationError):
+                    st.error('WARNING! You have not loaded a valid API Key', icon="🚨")
+                if isinstance(e, openai.RateLimitError):
+                    st.error('WARNING! You do not have any money loaded to your API key', icon="🚨")
+                if isinstance(e, openai.APIError):
+                    st.error('WARNING! You have made an API error', icon="🚨")
+                if isinstance(e, openai.APIConnectionError):
+                    st.error('WARNING! The connection to the API failed', icon="🚨")
+                else:
+                    st.error('WARNING! An unknown error occurred', icon="🚨")
 
     # Enter user response into conversation
     if user_response := st.chat_input("What's your response?"):
         st.session_state.messages.append({"role": "user", "content": user_response})
 
         # Select agent
-        agent = None
-        conductor_agent = ConductorAgent(llm = st.session_state['llm'])
-        with st.spinner('EthicsBot is thinking...'):
-            agent_id = conductor_agent.select_agent(messages = st.session_state.messages)
-        agent_action = ''
-        if agent_id == 1:
-            agent = UserClarificationAgent(llm = st.session_state['llm'])
-            agent_action = "trying to figure out what you mean..."
-        elif agent_id == 2:
-            agent = ScenarioClarificationAgent(llm = st.session_state['llm'])
-            agent_action = "trying to provide more information about the scenario..."
-        elif agent_id == 3:
-            agent = RetortAgent(llm = st.session_state['llm'])
-            agent_action = "retorting..."
-        elif agent_id == 4:
-            agent = InjectionAttackAgent(llm = st.session_state['llm'])
-            agent_action = "skeptical of your response..."
-        if agent is not None:
-            agent.system_prompt_ += prompt_modifier(st.session_state['username'])
+        try:
+            agent = None
+            conductor_agent = ConductorAgent(llm = st.session_state['llm'])
+            with st.spinner('EthicsBot is thinking...'):
+                agent_id = conductor_agent.select_agent(messages = st.session_state.messages)
+            agent_action = ''
+            if agent_id == 1:
+                agent = UserClarificationAgent(llm = st.session_state['llm'])
+                agent_action = "trying to figure out what you mean..."
+            elif agent_id == 2:
+                agent = ScenarioClarificationAgent(llm = st.session_state['llm'])
+                agent_action = "trying to provide more information about the scenario..."
+            elif agent_id == 3:
+                agent = RetortAgent(llm = st.session_state['llm'])
+                agent_action = "retorting..."
+            elif agent_id == 4:
+                agent = InjectionAttackAgent(llm = st.session_state['llm'])
+                agent_action = "skeptical of your response..."
+            else:
+                agent = RetortAgent(llm = st.session_state['llm'])
+                agent_action = "retorting..."
 
-        for message in st.session_state.messages:
-            if message["role"].lower().strip() != 'system':
-                if message["role"] == "user":
-                    use_avatar = st.session_state['user_avatar']
+            if agent is not None:
+                agent.system_prompt_ += prompt_modifier(st.session_state['username'])
+
+            for message in st.session_state.messages:
+                if message["role"].lower().strip() != 'system':
+                    if message["role"] == "user":
+                        use_avatar = st.session_state['user_avatar']
+                    else:
+                        use_avatar = "⚖️"
+                    with st.chat_message(message["role"], avatar=use_avatar):
+                        st.write(message["content"])
+
+            # Generate agent response
+            with st.spinner(f'EthicsBot is {agent_action}'):
+                able_to_respond = False
+                for i in range(3):
+                    try:
+                        agent_response = agent.respond(messages = st.session_state.messages)
+                        able_to_respond = True
+                        break
+                    except:
+                        pass
+                if able_to_respond == False:
+                    agent_response = "I'm sorry, I'm having trouble responding. Please try again."
+
+                st.session_state.messages.append({"role": "assistant", "content": agent_response}) 
+
+            # Add to chat log
+            with st.chat_message("assistant", avatar = "⚖️"):
+                if agent_response:
+                    st.write(agent_response)
                 else:
-                    use_avatar = "⚖️"
-                with st.chat_message(message["role"], avatar=use_avatar):
-                    st.write(message["content"])
+                    st.write("I'm sorry, I'm having trouble responding. Please try again.")
 
-        # Generate agent response
-        with st.spinner(f'EthicsBot is {agent_action}'):
-            agent_response = agent.respond(messages = st.session_state.messages)
-            st.session_state.messages.append({"role": "assistant", "content": agent_response}) 
-
-        # Add to chat log
-        with st.chat_message("assistant", avatar = "⚖️"):
-            st.write(agent_response)
+        except Exception as e:
+            # if e is openAI.AuthenticationError, then prompt user to enter API key
+            if isinstance(e, openai.AuthenticationError):
+                st.error('WARNING! You have not loaded a valid API Key', icon="🚨")
+            elif isinstance(e, openai.RateLimitError):
+                st.error('WARNING! You do not have any money loaded to your API key', icon="🚨")
+            elif isinstance(e, openai.APIError):
+                st.error('WARNING! You have made an API error', icon="🚨")
+            elif isinstance(e, openai.APIConnectionError):
+                st.error('WARNING! The connection to the API failed', icon="🚨")
+            else:
+                st.error('WARNING! An unknown error occurred', icon="🚨")
